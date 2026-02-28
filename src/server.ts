aeac332cd5ec6b04714684a6958a8ed5e58c12ff
AlbertHerrera10 <albert.fco10@gmail.com>
2026-02-28 17:56:57 -0400

    fix Cors

--- src/server.ts
+++ src/server.ts
@@ -22,11 +22,11 @@ connectDB()
 const server = express()
 
 //Permitir conexiones
-const corsOptions : CorsOptions = {
+const corsOptions: CorsOptions = {
     origin: function(origin, callback){
-        if(origin=== process.env.FRONTEND_URL){
-            callback(null,true)
-        }else{
+        if(!origin || origin === process.env.FRONTEND_URL){
+            callback(null, true)
+        } else {
             callback(new Error('Error de CORS'))
         }
     }
