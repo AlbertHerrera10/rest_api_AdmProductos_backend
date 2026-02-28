48f94cbd90581ca5736bbbfdb05b7f5f95df14d7
AlbertHerrera10 <albert.fco10@gmail.com>
2026-02-28 17:49:11 -0400

    fix: include pg in db.ts

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,17 +1,19 @@
 import dotenv from "dotenv";
-dotenv.config() 
+dotenv.config()
 import { Sequelize } from "sequelize-typescript";
+import pg from "pg"; // 👈 importa pg explícitamente
 
-const db = new Sequelize(process.env.DATABASE_URL!,{
-    dialect: 'postgres',
+const db = new Sequelize(process.env.DATABASE_URL!, {
+  dialect: 'postgres',
+  dialectModule: pg, // 👈 esto le dice a Sequelize que use el pg importado
   dialectOptions: {
     ssl: {
       require: true,
-      rejectUnauthorized: false // necesario para Supabase
+      rejectUnauthorized: false
     }
   },
-    models: [__dirname + '/../models/**/*'],
-    logging: false
+  models: [__dirname + '/../models/**/*'],
+  logging: false
 })
 
 export default db
